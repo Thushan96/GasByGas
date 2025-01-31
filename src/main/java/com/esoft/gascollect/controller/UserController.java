9da8ae10b82b4f3b41604df3f2e31844c59ffe44
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-31 20:12:24 +0530

    update commit

--- src/main/java/com/esoft/gascollect/controller/UserController.java
+++ src/main/java/com/esoft/gascollect/controller/UserController.java
@@ -68,7 +68,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasAnyRole('USER','ADMIN', 'MANAGER')")
+  //  @PreAuthorize("hasAnyRole('USER','ADMIN', 'MANAGER')")
     public ResponseEntity<UserDTO> updateUser(@PathVariable int id, @RequestBody UserDTO userDTO) {
         Optional<UserDTO> updatedUser = userService.updateUser(id, userDTO);
         return updatedUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
